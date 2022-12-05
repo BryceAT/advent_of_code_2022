f82036d6a3418fc28342ebbedc1c019aaffc7f81
Bryce <bryce.terwilliger@gmail.com>
2022-12-05 01:04:41 -0500

    problem 5

--- src/main.rs
+++ src/main.rs
@@ -179,24 +179,92 @@ fn p4_1() -> i32 {
 fn p4_2() -> i32 {
     let mut tot: i32 = 0;
     let lines = io::BufReader::new(File::open("data/p4_1.txt").unwrap()).lines();
-    for line in lines {
-        if let Ok(x) = line {
-            let row:Vec<_> = x.chars().collect();
-            let mut i = 0;
-            let mut quad = vec![0;4];
-            for c in row {
-                match c.to_digit(10) {
-                    Some(d) => {quad[i] = quad[i] * 10 + d},
-                    None => {i += 1;}
-                }
+    for line in lines.filter_map(|x| x.ok()) {
+        let row:Vec<_> = line.chars().collect();
+        let mut i = 0;
+        let mut quad = vec![0;4];
+        for c in row {
+            match c.to_digit(10) {
+                Some(d) => {quad[i] = quad[i] * 10 + d},
+                None => {i += 1;}
             }
-            if quad[0].max(quad[2]) <= quad[1].min(quad[3]) {
-                tot += 1;
-            }
+        }
+        if quad[0].max(quad[2]) <= quad[1].min(quad[3]) {
+            tot += 1;
         }
     }
     tot
 }
+#[allow(dead_code)]
+fn p5_1() -> String {
+    let mut crates = vec![Vec::new(),
+    vec!['N','C','R','T','M','Z','P'],
+    vec!['D','N','T','S','B','Z'],
+    vec!['M','H','Q','R','F','C','T','G'],
+    vec!['G','R','Z'],
+    vec!['Z','N','R','H'],
+    vec!['F','H','S','W','P','Z','L','D'],
+    vec!['W','D','Z','R','C','G','M'],
+    vec!['S','J','F','L','H','W','Z','Q'],
+    vec!['S','Q','P','W','N']];
+
+    let lines = io::BufReader::new(File::open("data/p5_1.txt").expect("file not found")).lines();
+    for line in lines.filter_map(|x| x.ok()) {
+        if !line.starts_with("move") {
+            continue
+        }
+        let row:Vec<_> = line.chars().collect();
+        let mut i = 0;
+        let mut triple = vec![0;3];
+        for c in row {
+            match c.to_digit(10) {
+                Some(d) => {triple[i] = triple[i] * 10 + d},
+                None if triple[i] > 0 => {i += 1;},
+                _ => ()
+            }
+        }
+        for _ in 0..triple[0]{
+            let c = crates[triple[1] as usize].pop().unwrap();
+            crates[triple[2] as usize].push(c);
+        }
+    }
+    (1..10).map(|i| crates[i].last().unwrap()).collect()
+}
+#[allow(dead_code)]
+fn p5_2() -> String {
+    let mut crates = vec![Vec::new(),
+    vec!['N','C','R','T','M','Z','P'],
+    vec!['D','N','T','S','B','Z'],
+    vec!['M','H','Q','R','F','C','T','G'],
+    vec!['G','R','Z'],
+    vec!['Z','N','R','H'],
+    vec!['F','H','S','W','P','Z','L','D'],
+    vec!['W','D','Z','R','C','G','M'],
+    vec!['S','J','F','L','H','W','Z','Q'],
+    vec!['S','Q','P','W','N']];
+
+    let lines = io::BufReader::new(File::open("data/p5_1.txt").expect("file not found")).lines();
+    for line in lines.filter_map(|x| x.ok()) {
+        if !line.starts_with("move") {
+            continue
+        }
+        let row:Vec<_> = line.chars().collect();
+        let mut i = 0;
+        let mut triple = vec![0_usize;3];
+        for c in row {
+            match c.to_digit(10) {
+                Some(d) => {triple[i] = triple[i] * 10 + d as usize},
+                None if triple[i] > 0 => {i += 1;},
+                _ => ()
+            }
+        }
+        let take_ind =  crates[triple[1]].len() - triple[0];
+        let mut stack:Vec<_> = crates[triple[1]].drain(take_ind..).collect();
+        crates[triple[2]].append(&mut stack);
+    }
+    (1..10).map(|i| crates[i].last().unwrap()).collect()
+}
 fn main() {
-    println!("{}",p4_2());
+    println!("{}",p5_1());
+    println!("{}",p5_2());
 }
